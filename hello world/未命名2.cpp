b3a6f84d61a042fb4aa752ceafc15a5c6ef7b323
feilink <feilink@bukaopu.com>
2026-04-30 20:06:35 +0800

    v2

---
+++ hello world/未命名2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    char name[50];
+    
+
+    printf("ÇëÊäÈëÄãµÄÃû×Ö£º");
+    
+ 
+    scanf("%s", name);
+    
+   
+    printf("Hello, %s!\n", name);
+    
+    return 0;
+}
